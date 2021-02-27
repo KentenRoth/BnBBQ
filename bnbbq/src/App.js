b836651c8618ac823273bd48ca91e9921fee5333
Kent Roth <kentencroth@yahoo.com>
2021-02-27 08:14:39 -0600

    Getting posts and passing as props

--- bnbbq/src/App.js
+++ bnbbq/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import NavBar from './Components/Nav';
@@ -7,10 +8,14 @@ import NavBar from './Components/Nav';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			posts: [],
+		};
 	}
 
-	componentDidMount() {}
+	componentDidMount() {
+		this.getPosts();
+	}
 
 	render() {
 		return (
@@ -18,12 +23,20 @@ class App extends React.Component {
 				<NavBar />
 				<BrowserRouter>
 					<Switch>
-						<Route path="/" component={LandingPage} exact />
+						<Route path="/" exact>
+							<LandingPage posts={this.state.posts} />
+						</Route>
 					</Switch>
 				</BrowserRouter>
 			</div>
 		);
 	}
+
+	getPosts = () => {
+		axios.get('/posts').then((response) => {
+			this.setState({ posts: response.data });
+		});
+	};
 }
 
 export default App;
--- bnbbq/src/Components/LandingPage/LandingPage.js
+++ bnbbq/src/Components/LandingPage/LandingPage.js
@@ -3,6 +3,9 @@ import Header from './Header';
 import FeaturedPosts from './FeaturedPosts';
 
 class LandingPage extends React.Component {
+	componentDidMount() {
+		console.log(this.props);
+	}
 	render() {
 		return (
 			<div>
