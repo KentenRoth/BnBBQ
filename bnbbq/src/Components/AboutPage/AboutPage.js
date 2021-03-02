a72f3db934e1e960c7c7c73cd8f0145eee28f35e
Kent Roth <kentencroth@yahoo.com>
2021-03-02 12:13:28 -0600

    About Page completed

--- bnbbq/src/Components/AboutPage/AboutPage.js
+++ bnbbq/src/Components/AboutPage/AboutPage.js
@@ -2,8 +2,74 @@ import React from 'react';
 
 const AboutPage = () => {
 	return (
-		<div>
-			<h1>About Page</h1>
+		<div className="aboutContent">
+			<div className="aboutContainer">
+				<div className="title about">
+					<h1>About Page</h1>
+				</div>
+				<div className="aboutTextCard">
+					<h1>My Beard</h1>
+					<br />
+					<p>
+						My beard journey has started and restarted several
+						times. I have let it grow out only to shave it because
+						it is a rough, tangled mess. After a while, I decided to
+						look into beard products, and the ones I found early on
+						didn't work out for me. The skin under my beard would
+						become flakey and hurt. I decided that maybe big box
+						store products were not for me and started to research
+						smaller companies. Once I got into small-batch
+						companies, my beard thanked me. I had fewer bad beard
+						days, and the skin under my beard was no longer flakey
+						and sore. Fixing just those two things was huge for me,
+						but the added scents and having my beard feel soft was
+						another plus. My wife and daughter always complained
+						about my "pokey" beard, but after using better quality
+						products, that complaint has gone away. I started with
+						just a couple of different scents from one company, and
+						now my collection has grown, and I have several other
+						companies that I plan on adding to the collection.
+					</p>
+					<p>
+						When it comes to my preferences, I like vanilla, citrus,
+						and coffee scents. I lean towards a simple scent most of
+						the time, but I have come across some more complex
+						scents that I do enjoy.{' '}
+					</p>
+				</div>
+				<br />
+				<div className="aboutTextCard">
+					<h1>My Barbeque</h1>
+					<br />
+					<p>
+						It was well after moving out of my parent's house before
+						I learned how to cook. My cooking skills on the grill
+						were hotdogs and burgers; anything else was undercooked
+						or overcooked. A few years back, I started to take
+						cooking and grilling a little more seriously and found
+						out that it is something that I enjoy doing. I started
+						with a regular gas grill and used that for several
+						years. It was only a couple of years ago that I began to
+						teach myself how to smoke different cuts. I started with
+						an electric smoker, and now I have worked up to a pellet
+						smoker. My next step will be adding a charcoal smoker to
+						what I have. I'm not a chef by far, and I don't ever see
+						myself going to a smoking competition. I like to cook
+						for family and, on occasion, have some friends over and
+						have a barbeque. The best part of cooking is always
+						trying new things and mixing different stuff to perfect
+						a seasoning.
+					</p>
+					<p>
+						My preferences for grilling are pork ribs, chicken, and
+						salmon. I grew up on a cattle farm in Iowa, so beef and
+						steaks usually aren't on my list of top things to throw
+						on the grill. I had many hamburgers and steaks growing
+						up, so I typically don't pass it up when I can get
+						chicken or pork.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
