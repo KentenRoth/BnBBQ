c6b22da99393f874bfaeba64735ed45a1fb60aea
Kent Roth <kentencroth@yahoo.com>
2021-03-07 14:07:52 -0600

    Setting up bbq post

--- bnbbq/src/Components/PostPage/BBQPost.js
+++ bnbbq/src/Components/PostPage/BBQPost.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import Ad from '../Ad/Ad';
 
 class BBQPost extends React.Component {
-	constructor(props) {
-		super(props);
+	componentDidMount() {
+		console.log(this.props.post);
 	}
-
-	componentDidMount() {}
 
 	render() {
 		return (
-			<div>
-				<h1>This is a BBQ Post</h1>
+			<div className="pageContent">
+				<div className="container">
+					<div className="postContainer">
+						<div className="postImage"></div>
+						<div className="postTitle">
+							<h2>{this.props.post.title}</h2>
+							<Ad />
+						</div>
+						<div>
+							{this.props.post.content.map((para, i) => {
+								return <p key={i}>{para}</p>;
+							})}
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
