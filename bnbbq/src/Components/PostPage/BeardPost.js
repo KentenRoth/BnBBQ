133ac925e3fd74457ec9c70efe4ac324f8eab3f4
Kent Roth <kentencroth@yahoo.com>
2021-03-07 15:41:43 -0600

    Setup beard post

--- bnbbq/src/Components/PostPage/BeardPost.js
+++ bnbbq/src/Components/PostPage/BeardPost.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ad from '../Ad/Ad';
 
 class BeardPost extends React.Component {
 	constructor(props) {
@@ -8,9 +9,55 @@ class BeardPost extends React.Component {
 	componentDidMount() {}
 
 	render() {
+		const image = this.props.post.f_image;
+		var date = new Date(this.props.post.created);
+		var year = date.getFullYear();
+		var month = ('0' + (date.getMonth() + 1)).slice(-2);
+		var day = ('0' + date.getDate()).slice(-2);
 		return (
-			<div>
-				<h1>This is a beard Post</h1>
+			<div className="pageContent">
+				<div className="container">
+					<div className="postContainer">
+						<div className="postImage">
+							<img
+								src={`${process.env.PUBLIC_URL}/assets/images/${image}`}
+								alt={image}
+							/>
+						</div>
+						<div className="postTitle">
+							<h2>{this.props.post.title}</h2>
+							<p>Location: {this.props.post.location}</p>
+							<p>
+								Posted: {month}/{day}/{year}
+							</p>
+							<Ad />
+						</div>
+						<div>
+							{this.props.post.content.map((para, i) => {
+								return <p key={i}>{para}</p>;
+							})}
+						</div>
+						<div>
+							<p>Beard oil ingredients</p>
+							<ul>
+								{this.props.post.ingredients.map((ing, i) => {
+									return <li key={i}>{ing}</li>;
+								})}
+							</ul>
+						</div>
+						<div>
+							<p>Beard butter ingredients</p>
+							<ul>
+								{this.props.post.ingredients2.map((ing, i) => {
+									return <li key={i}>{ing}</li>;
+								})}
+							</ul>
+						</div>
+						<div>
+							<Ad />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
