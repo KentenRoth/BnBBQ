f247e8db25d92a19c26a7afd8329df86c5061d74
Kent Roth <kentencroth@yahoo.com>
2021-03-24 12:54:56 -0500

    made logo link

--- bnbbq/src/Components/Nav/Nav.js
+++ bnbbq/src/Components/Nav/Nav.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Burger from './Burger';
 
+import { Link } from 'react-router-dom';
+
 class NavBar extends React.Component {
 	render() {
 		return (
 			<div className="nav">
 				<div className="navLogo">
-					<img
-						src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
-						alt="logo"
-					/>
+					<Link to="/">
+						<img
+							src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
+							alt="logo"
+						/>
+					</Link>
 				</div>
 				<Burger />
 			</div>
